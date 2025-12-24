73356e29f8fda7b0791b2f730b64f68d6aff7c10
Leandro <leandro1992@gmail.com>
2025-12-23 22:04:48 -0300

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,11 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+        <link rel="icon" type="image/x-icon" href="/logo.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo-icon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo-icon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/logo-icon.png" />
-        <meta name="theme-color" content="#0F3B75" />
+        <meta name="theme-color" content="#0ea5e9" />
         <meta name="description" content="Sistema de gerenciamento de partidas de dominó do Gardenal" />
       </Head>
       <body>
